ae46851e2aa22785109eb052ffc45a7a407d8797
rickyspanish2112 <ross.starkey@outlook.com>
2022-01-08 11:46:05 +0000

    CompletedWorkingWithDynamicContent

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,20 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../utils/path') 
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
+
+
 
 module.exports = router;
