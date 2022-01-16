06128a4b255a3d9a9186999f3262700fcbd92d34
rickyspanish2112 <ross.starkey@outlook.com>
2022-01-16 10:11:53 +0000

    implemented MVC pattern

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.engine(
@@ -20,23 +22,15 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-
-
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-
-    res.status(404).render('404', {pageTitle : '404 Not found'});
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    // res.status(404).send('<h1>Page not found</h1>')
-})
+app.use(errorController.getPageNotFound);
 
 app.listen(3002);
--- routes/shop.js
+++ routes/shop.js
@@ -1,24 +1,9 @@
-const path = require('path')
-
 const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const rootDir = require('../utils/path') 
-const adminData = require('./admin')
-
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
-
-
+router.get('/', productsController.getProducts);
 
 module.exports = router;
